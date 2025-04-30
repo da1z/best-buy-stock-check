202aac4c5c544d86a56daa68c0fe9ac1e4f9611b
kiryl <da1z@mail.ru>
2025-04-30 13:59:30 -0400

    test with secret

--- api/check.ts
+++ api/check.ts
@@ -2,7 +2,11 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query;
+  const {
+    body: { secret },
+  } = req;
   return res.json({
     message: `Hello ${name}!`,
+    secret,
   });
 }
